5bc0e16a0d779a7efe64c0aa878bff3ce1681e3c
Eliel-KOBI <elielkobi47@gmail.com>
2025-02-11 20:00:24 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+document.getElementById("ctaButton").addEventListener("click", function() {
+    
+    alert("Bienvenue dans mon univers ! Découvrez mes passions et projets.");
+});
+
+document.getElementById("passion1").addEventListener("click", function() {
+    alert("Vous avez cliqué sur la passion Finance !");
+});
+
+document.getElementById("passion2").addEventListener("click", function() {
+    alert("Vous avez cliqué sur la passion Informatique !");
+});
